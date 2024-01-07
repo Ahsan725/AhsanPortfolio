f98555575b1fc9cb6f2ea3117c69b7c4c5abaebf
Ahsan Baseer <125620778+Ahsan725@users.noreply.github.com>
2024-01-06 19:37:15 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,82 @@
+var typed = new Typed(".text", {
+  strings: [
+    "a Software Engineer.",
+    "a Frontend Developer.",
+    "a Backend Developer.",
+    "an IOS Developer.",
+    "a UI / UX Designer.",
+    "an AI / ML Engineer.",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 3000,
+  loop: true,
+});
+
+var typed1 = new Typed(".text1", {
+  strings: [
+    "Java.",
+    "Python.",
+    "HTML.",
+    "JavaScript.",
+    "TypeScript",
+    "CSS.",
+    "React.",
+    "Node.js.",
+    "Express.js.",
+    "MySQL.",
+    "PHP.",
+    "C++.",
+    "Swift.",
+    "Bootstrap.",
+    "API.",
+    "React JS.",
+    "Redux.",
+    "Next JS.",
+    "Node JS.",
+    "Figma.",
+    "UI / UX.",
+    "Tailwind.",
+    "Auth JS.",
+    "OAuth.",
+    "Clerk.",
+    "NoSQL.",
+    "DBMS.",
+    "ORM.",
+    "AWS.",
+    "MongoDB.",
+    "Git.",
+    "GitHub.",
+    "Continuous Integration(CI).",
+    "Continuous Delivery(CD).",
+    "Test Driven Development(TDD).",
+    "Problem Solving.",
+    "Artificial Intelligence.",
+    "Machine Learning.",
+    "SaaS.",
+    "Object Oriented Programming.", 
+    "RESTful APIs.",
+    "IOS.",
+    "Android.",
+    "HTTP.",
+    "Xcode.",
+
+
+    
+
+  ],
+  typeSpeed: 100,
+  backSpeed: 70,
+  backDelay: 3000,
+  loop: true,
+});
+
+var typed2 = new Typed(".text2", {
+  strings: [
+    "Ahsan Baseer.",
+  ],
+  typeSpeed: 400,
+  backSpeed: 100,
+  backDelay: 3000,
+  loop: false,
+});
